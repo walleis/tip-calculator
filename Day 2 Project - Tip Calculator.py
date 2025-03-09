3599c38ab17501b3697d5170698d679b577557d6
Wallace Rodrigues <wallacerster1@gmail.com>
2025-03-09 09:50:42 -0300

    Add files via upload

---
+++ Day 2 Project - Tip Calculator.py
@@ -0,0 +1,8 @@
+#This program uses math operators to do the convertion for how much each person will pay.
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill?\nR$"))
+tip = int(input("What percentage tip would you like to give? 10, 12 or 15?\n"))
+people = int(input("How many people to split the bill?\n"))
+billwithtip = (tip / 100 * bill + bill) / people
+finalresult = round(billwithtip, 2)
+print(f"Each person will pay R${finalresult}.")
